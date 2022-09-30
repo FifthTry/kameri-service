dc0058bbfb93e4b8d923fb94431610447339369e
abrark <abrark.asahi@gmail.com>
2022-09-30 13:28:41 +0530

    added add-todo mock api

--- dj/todo/views.py
+++ dj/todo/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import django
+import json
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+@csrf_exempt
+def add_todo(req: django.http.HttpRequest):
+    # It should be only POST request
+    # Get data from request body and add it to database
+    # It will redirect to today's todo page
+
+    if req.method == "GET":
+        return django.http.HttpResponse("Wrong Method GET", status=405)
+
+    body = json.loads(req.body.decode("utf-8"))
+    print(body)
+
+    return django.http.JsonResponse({
+        "id": "<todo id>",
+    })
+
+"""
+curl -X POST \
+--data '{"title": "Take update from Interns"}' \
+http://127.0.0.1:8001/api/add-todo/
+"""
