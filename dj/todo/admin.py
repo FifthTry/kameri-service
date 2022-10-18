a0fb03646b045b2eab95da08de4e51e98f7c1363
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-10-18 23:06:37 +0530

    Added TODO model in admin site page

--- dj/todo/admin.py
+++ dj/todo/admin.py
@@ -3,4 +3,8 @@ from .models import Todo
 
 # Register your models here.
 
-admin.site.register(Todo)
+# admin.site.register(Todo)
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Todo._meta.get_fields()]
