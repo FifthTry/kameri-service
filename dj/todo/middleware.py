8e79c82cd51655c389e3411bf834cb8f636e41dd
abrark <abrark.asahi@gmail.com>
2022-10-20 21:43:37 +0530

    fixed middleware

--- dj/todo/middleware.py
+++ dj/todo/middleware.py
@@ -1,14 +1,31 @@
 from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 
-class TrackerMiddleware(MiddlewareMixin):
-    def process_response(self, request, response: JsonResponse):
-        if not request.COOKIES.get('tracker_id', None):
-            import datetime
 
+#
+"""
+- tracker handle
+- epoch
+- add entry to table
+"""
+
+import time
+import math
+
+class TrackerMiddleware(MiddlewareMixin):
+
+    def process_request(self, request):
+        if request.COOKIES.get('tid', None) is None:
+            request.tid = math.floor(time.time())
+        else:
+            request.tid = int(request.COOKIES.get('tid'))
+        return None
+
+    def process_response(self, request, response: JsonResponse):
+        if not request.COOKIES.get('tid', None):
+            import datetime
             max_age = 365 * 24 * 60 * 60  # 10 years
             expires = datetime.datetime.now() + datetime.timedelta(seconds=max_age)
-            response.set_cookie('tracker_id', expires.strftime("%a, %d-%b-%Y %H:%M:%S GMT"), expires=expires.strftime("%a, %d-%b-%Y %H:%M:%S GMT"))
-
+            response.set_cookie('tid', str(request.tid), expires=expires.strftime("%a, %d-%b-%Y %H:%M:%S GMT"))
             print("process_response",response.cookies)
         return response
